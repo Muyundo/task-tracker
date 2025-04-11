6d2de4f6b6ccee7a10a41dd81c5190a6697bbeee
Muyundo <muyundobrian@gmail.com>
2025-04-11 09:43:39 +0300

    updated taskname to taskInput.value.trim()

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addTaskBtn = document.getElementById('addTaskBtn')
 const taskList = document.getElementById('taskList')
 
 addTaskBtn.addEventListener("click", () =>{
-    const taskName = taskInput.ariaValueMax.trim()
+    const taskName = taskInput.value.trim()
 
     if (taskName !== ""){
         const li = document.createElement("li")
