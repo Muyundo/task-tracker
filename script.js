6536c4406c134343f617b7fc42a066a68c035ca9
Muyundo <muyundobrian@gmail.com>
2025-04-11 11:30:54 +0300

    added script to handle task storage and status

--- script.js
+++ script.js
@@ -2,14 +2,41 @@ const taskInput = document.getElementById('taskInput')
 const addTaskBtn = document.getElementById('addTaskBtn')
 const taskList = document.getElementById('taskList')
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+renderTasks()
+
 addTaskBtn.addEventListener("click", () =>{
     const taskName = taskInput.value.trim()
 
-    if (taskName !== ""){
-        const li = document.createElement("li")
-        li.textContent = taskName
-        taskList.appendChild(li)
+    if (taskName !== "") {
+        const task = {
+          name: taskName,
+          done: false
+        };
+        tasks.push(task);
+        saveAndRender()
 
         taskInput.value = ""
     } 
 })
+
+function saveAndRender(){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    renderTasks()
+}
+
+function renderTasks() {
+    taskList.innerHTML = ""
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li")
+        li.textContent = task.name
+        if(task.done) li.classList.add("done")
+
+            li.addEventListener("click", () =>{
+                tasks[index].done = !tasks[index].done
+                saveAndRender()
+            })
+            taskList.appendChild(li)
+    })
+        
+}
