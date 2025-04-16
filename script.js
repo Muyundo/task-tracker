7784d25c083ec27d4e2022ce1441a402919c4b6b
Muyundo <muyundobrian@gmail.com>
2025-04-16 11:45:57 +0300

    added post method to push

--- script.js
+++ script.js
@@ -2,41 +2,36 @@ const taskInput = document.getElementById('taskInput')
 const addTaskBtn = document.getElementById('addTaskBtn')
 const taskList = document.getElementById('taskList')
 
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-renderTasks()
+//let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+//renderTasks()
 
 addTaskBtn.addEventListener("click", () =>{
     const taskName = taskInput.value.trim()
 
     if (taskName !== "") {
-        const task = {
-          name: taskName,
-          done: false
-        };
-        tasks.push(task);
-        saveAndRender()
+        fetch("add_task.php", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({name: taskName })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.success){
+                const li = document.createElement("li")
+                li.textContent = taskName
+                taskList.appendChild(li)
+                taskInput.value = ""
 
-        taskInput.value = ""
+            }else{
+                alert("Error: "+ data.message)
+            }
+        })
+        .catch(err => {
+            console.error("Fetch error:", err)
+        })
+
+   
     } 
 })
-
-function saveAndRender(){
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-    renderTasks()
-}
-
-function renderTasks() {
-    taskList.innerHTML = ""
-    tasks.forEach((task, index) => {
-        const li = document.createElement("li")
-        li.textContent = task.name
-        if(task.done) li.classList.add("done")
-
-            li.addEventListener("click", () =>{
-                tasks[index].done = !tasks[index].done
-                saveAndRender()
-            })
-            taskList.appendChild(li)
-    })
-        
-}
